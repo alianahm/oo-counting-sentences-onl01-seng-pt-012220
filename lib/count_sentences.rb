c5312b913f09cd3e8b873e9c55f481821971cf64
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2020-02-14 04:41:43 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,9 @@
 require 'pry'
 
 class String
-
+  
   def sentence?
-    
+    self.end_with?(".") ? true : false 
   end
-
-  def question?
-
-  end
-
-  def exclamation?
-
-  end
-
-  def count_sentences
-
-  end
-end
+  
+  
