9da9847f8d3d672908ced0942b233df5fafc8b61
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2020-02-14 04:45:23 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -12,13 +12,6 @@ class String
   
   def exclamation?
     self.end_with?("!")
-  end
   
-  def count_sentences
-    self.split(/[.!?]/).reject {|x| x.empty?}.size
-  end
   
-  def count_sentences_words
-    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
-  end 
   
